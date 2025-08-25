a1864393482585dc1b669d935b934d7825af4ba5
khaddy-girl <adebayokhaddy@gmail.com>
2025-08-25 16:46:58 +0100

    Added calculator gradle file

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -4,9 +4,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
-
+	final static double PI = 3.142;
 	public int sum(int a, int b) {
 		return a + b;
 	}
+
+	public static void subtract(int a, int b) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("yeah");
+		}
+
+	}
+
 }
 
